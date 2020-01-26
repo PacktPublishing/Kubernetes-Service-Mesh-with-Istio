7c0338dde777d93816ce239b3fb6dbc71ef01273
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-01-26 13:41:49 +0100

    Added Hello Istio v2 service.

---
+++ Section-2/services/hello-istio-golang-v2/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/api/hello", helloHandler)
+	http.HandleFunc("/api/v2/hello", helloHandler)
+
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Kubernetes Service Meshes with Istio by Packt Publising")
+}
+
+type hello struct {
+	Message string
+	Version string
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	m := hello{"Hello Istio from Golang.", "v2"}
+	b, err := json.Marshal(m)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(b)
+}
